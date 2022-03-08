fc8ae779ab762a64ac903920d1a6ddc0076160e0
Piotr Kowalski <piotr@pkowalski.pw>
2022-03-08 21:57:00 +0100

    Clear shift when surrounding word with brakcets

--- keyboards/preonic/keymaps/KowalskiPiotr98/word_surround.c
+++ keyboards/preonic/keymaps/KowalskiPiotr98/word_surround.c
@@ -28,6 +28,8 @@ void process_surround(uint16_t keycode, keyrecord_t *record)
         case BracketWrap:
             if (record->event.pressed)
             {
+                if (get_mods() & MOD_MASK_SHIFT)
+                    del_mods(MOD_MASK_SHIFT);
                 SEND_STRING(SS_LCTL(SS_TAP(X_LEFT)) SS_LSFT(SS_TAP(X_9)) SS_LCTL(SS_TAP(X_RGHT)) SS_LSFT(SS_TAP(X_0)) SS_TAP(X_LEFT));
             }
             break;
