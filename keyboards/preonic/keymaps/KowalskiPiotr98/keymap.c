b3e3a30d2d086cdcc518d92339c2a099d5d58863
Piotr Kowalski <piotr@pkowalski.net>
2022-12-02 15:30:17 +0100

    Replace the newly split spacebar with shift

--- keyboards/preonic/keymaps/KowalskiPiotr98/keymap.c
+++ keyboards/preonic/keymaps/KowalskiPiotr98/keymap.c
@@ -7,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,
         KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, MO(2), MO(1), KC_SPC, KC_SPC, MO(3), MO(4), KC_RALT, KC_RCTL, KC_ENT),
+        KC_LCTL, KC_LGUI, KC_LALT, MO(2), MO(1), KC_SPC, KC_RSFT, MO(3), MO(4), KC_RALT, KC_RCTL, KC_ENT),
 	[1] = LAYOUT_ortho_5x12(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, KC_WH_U, KC_WH_L, KC_WH_R, _______, _______, _______,
