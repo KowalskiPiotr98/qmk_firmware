01cd737387664ec43e2dd53f3b8ab454ccad7f9c
Piotr Kowalski <piotr@pkowalski.net>
2023-06-18 15:01:42 +0200

    Add Moonlander initial layout

---
+++ keyboards/moonlander/keymaps/KowalskiPiotr98/common.c
@@ -0,0 +1,14 @@
+#pragma once
+#include QMK_KEYBOARD_H
+
+enum custom_keycodes {
+    Lambda = SAFE_RANGE,
+    Nequal,
+    EscQuot,
+    InnerSel,
+    Shrug,
+    WinSwap,
+    LCTL,
+    LGUI
+};
+
