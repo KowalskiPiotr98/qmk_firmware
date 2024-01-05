a7151e5c382168abafca5be65e9f2893a1e53675
Piotr Kowalski <piko98@proton.me>
2024-01-05 10:58:37 +0100

    Add own keymap for k3-pro

---
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/KowalskiPiotr98/config.h
@@ -0,0 +1,7 @@
+/* Custom mousekey config */
+#define MK_KINETIC_SPEED
+#define MOUSEKEY_INITIAL_SPEED 20
+#define MK_VARIANT MK_TYPE_KINETIC
+#define MK_KINETIC_WHEEL_ACCN 4
+
+#define RGBLIGHT_DEFAULT_ON false
